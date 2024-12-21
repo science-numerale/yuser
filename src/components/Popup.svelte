fe31cba5242d99be482dec727ff7e29b98706e2b
science-numerale <example@example.com>
2024-12-21 15:10:56 +0100

    Quelques modifs...

--- src/components/Popup.svelte
+++ src/components/Popup.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 let { dialogue = $bindable(), children } = $props()
+function handleKeydown(event: KeyboardEvent) {
+	if (event.key === 'Escape') {
+		event.preventDefault();  // Prevent the default action (closing the dialog)
+	}
+}
 </script>
 
-<dialog bind:this={dialogue}>
+<dialog bind:this={dialogue} onkeydowncapture={handleKeydown}>
 	{@render children()}
 </dialog>
