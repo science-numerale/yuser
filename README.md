3223f5a90ae09301417722703fe09f775e13392c
science-numerale <191532220+science-numerale@users.noreply.github.com>
2024-12-21 01:49:03 +0100

    ... voilà

--- README.md
+++ README.md
@@ -1,45 +1,12 @@
+# Commandes
+
+`nix-shell` (sur Nix) ou installer deno et nodejs
+`deno run dev` pour démarer Vite (le serveur de dev)
+`deno run build` pour générer une page statique dans `build/`
+
 # Progression:
 
  - [x] page d'accueil
  - [x] page d'erreur 404
  - [ ] pied de page
  - [ ] en-tête
-
-# sv
-
-Everything you need to build a Svelte project, powered by [`sv`](https://github.com/sveltejs/cli).
-
-## Creating a project
-
-If you're seeing this, you've probably already done this step. Congrats!
-
-```bash
-# create a new project in the current directory
-npx sv create
-
-# create a new project in my-app
-npx sv create my-app
-```
-
-## Developing
-
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
-
-```bash
-npm run dev
-
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
-```
-
-## Building
-
-To create a production version of your app:
-
-```bash
-npm run build
-```
-
-You can preview the production build with `npm run preview`.
-
-> To deploy your app, you may need to install an [adapter](https://svelte.dev/docs/kit/adapters) for your target environment.
--- src/components/PopupEnnuyeux.svelte
+++ src/components/PopupEnnuyeux.svelte
@@ -1,9 +1,46 @@
-<script>
+<script lang="ts">
+import type { Snippet } from "svelte";
 import Popup from "./Popup.svelte";
+let { dialogue = $bindable(), children }: { dialogue: HTMLDialogElement | undefined, children: Snippet } = $props()
 
-let { dialogue: dialogue = $bindable(), children } = $props()
+let dialogue_confirmation: HTMLDialogElement | undefined = $state()
+let timer = $state({v:0})
+let beau_timer = $derived(timer.v.toFixed(2).toString()) // Sinon on a le bug du 0.1 + 0.2
+let faux_timer = $derived((timer.v + 1).toFixed(2).toString())
+let intervale: number;
+let random: { v: boolean } = $state({ v: false});
+
+function je_veux_fermer() {
+	random.v = Math.random() < 0.5
+	dialogue_confirmation?.showModal()
+	timer.v = 0
+	clearInterval(intervale)
+	intervale = setInterval(()=>{timer.v += 0.01}, 10)
+}
+function fermer() {
+	dialogue_confirmation?.close()
+}
+function fermer_tout() {
+	fermer()
+	dialogue?.close()
+}
 </script>
 
-<Popup bind:dialog={dialogue}>
+<Popup bind:dialogue>
 	{@render children()}
+	<br>
+	<button onclick={je_veux_fermer}><abbr title="Ne Pas Laisser Ce Popup Ouvert">NPLCPO</abbr></button>
 </Popup>
+
+<Popup bind:dialogue={dialogue_confirmation}>
+	Êtes-vous sûr de vouloir fermer ce popup ainsi que celui sur lequel vous avez cliqué il y a {beau_timer} secondes
+	{@render faux_bouton(random.v)}
+	<button onclick={fermer_tout}>Autant que {beau_timer} = {beau_timer}</button>
+	{@render faux_bouton(!random.v)}
+</Popup>
+
+{#snippet faux_bouton(affiché: boolean)}
+	{#if affiché}
+		<button onclick={fermer}>Autant que {faux_timer} = {beau_timer}</button>
+	{/if}
+{/snippet}
