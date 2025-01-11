523b8d10f484ef1bd51b3b81c83b7dfb95b97536
Merlin <merlin.aitm@proton.me>
2025-01-11 13:42:20 +0100

    Ajout de la page repos

---
+++ src/routes/repos/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Clavier from "./Clavier.svelte";
+	import Humour from "./Humour.svelte";
+	import Libre from "./Libre.svelte";
+	import Patate from "./Patate.svelte";
+	import Yenterprise from "./Yenterprise.svelte";
+
+	// TODO : Faire un peu plus proprement, jsp si une liste de composant c'est super...
+	let elems = [Patate, Libre, Clavier, Yenterprise, Humour];
+
+	let SubPage = elems[Math.floor(Math.random() * elems.length)];
+</script>
+
+<SubPage />
